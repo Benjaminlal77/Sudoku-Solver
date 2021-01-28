f5d47435da54aa1a566f3feedf0d54b6996ada77
Benjamin Lal <68187672+Benjaminlal77@users.noreply.github.com>
2021-01-28 00:28:51 -0500

    Created check_events.py
    Used to handle events in the game.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import sys
 import pygame
 from settings import Settings
+from check_events import check_events
 
 pygame.init()
 screen = pygame.display.set_mode((Settings.screen_width, Settings.screen_height))
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()  
-            
+    check_events()
     pygame.display.flip()
